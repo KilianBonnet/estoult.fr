2b407a93ad89d3dcf6c14b09c48950caea68760a
Kilian Bonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2023-12-08 22:23:19 +0100

    Adding certification

--- docker-helper/start-nginx-ssh.sh
+++ docker-helper/start-nginx-ssh.sh
@@ -9,8 +9,8 @@ until [ "$(curl -I -s http://localhost | head -n 1)" != "HTTP/1.1 200 OK" ]; do
 done
 
 # Exécuter la commande souhaitée (par exemple, une commande spécifique à votre application)
-echo "NGINX is ready. Executing the next command..."
-# Commande suivante ici
+echo "NGINX is ready."
+certbot --nginx --agree-tos --no-eff-email --email kilian.bonnet@estoult.fr -d estoult.fr --redirect --keep-until-expiring
 
 # Keeping container alive
 tail -f /dev/null
