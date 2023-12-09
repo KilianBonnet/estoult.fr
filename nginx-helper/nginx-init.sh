14860772c8807384ff85847e51d178ed5a74f52c
Kilian Bonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2023-12-09 16:17:25 +0100

    Site web cool

--- nginx-helper/nginx-init.sh
+++ nginx-helper/nginx-init.sh
@@ -5,6 +5,8 @@ if [ -z "$(ls -A /etc/letsencrypt/live/www.estoult.fr)" ]; then
   certbot certonly --standalone --agree-tos --no-eff-email --email kilian.bonnet@estoult.fr -d www.estoult.fr --redirect --keep-until-expiring
   rm /etc/cron.d/certbot-renew
 fi
+  echo www.estoult.fr will use cronjob autorenew
+else
 
 # Start Nginx
 nginx -g "daemon off;"
