86ffc886f7f3ddfc79c6f075cdc464dd4c1b7ebf
Kilian Bonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2023-12-09 16:11:19 +0100

    Test

--- nginx-helper/nginx-init.sh
+++ nginx-helper/nginx-init.sh
@@ -2,8 +2,8 @@
 
 # Check if /etc/letsencrypt/live/www.estoult.fr is empty
 if [ -z "$(ls -A /etc/letsencrypt/live/www.estoult.fr)" ]; then
-  echo "Empty /etc/letsencrypt/live/www.estoult.fr directory, running certbot certonly..."
   certbot certonly --standalone --agree-tos --no-eff-email --email kilian.bonnet@estoult.fr -d www.estoult.fr --redirect --keep-until-expiring
+  rm /etc/cron.d/certbot-renew
 fi
 
 # Start Nginx
