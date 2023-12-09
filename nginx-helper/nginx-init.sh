3101c92bb59d6b375fc20344c6ca0e5eedb0ae76
Kilian Bonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2023-12-09 16:19:50 +0100

    Adding estoult.fr certification

--- nginx-helper/nginx-init.sh
+++ nginx-helper/nginx-init.sh
@@ -8,5 +8,17 @@ fi
   echo www.estoult.fr will use cronjob autorenew
 else
 
+
+
+# Check if /etc/letsencrypt/live/estoult.fr is empty
+if [ -z "$(ls -A /etc/letsencrypt/live/estoult.fr)" ]; then
+  certbot certonly --standalone --agree-tos --no-eff-email --email kilian.bonnet@estoult.fr -d estoult.fr --redirect --keep-until-expiring
+  rm /etc/cron.d/certbot-renew
+fi
+  echo estoult.fr will use cronjob autorenew
+else
+
+
+
 # Start Nginx
 nginx -g "daemon off;"
