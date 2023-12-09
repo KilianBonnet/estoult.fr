64d4c0ca714ad2ada3c17c4e5696de790b107288
Kilian Bonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2023-12-09 21:58:24 +0100

    front_builder

--- build-front.sh
+++
@@ -1,6 +0,0 @@
-docker container stop estoult_fr-front
-docker container rm estoult_fr-front
-docker image rm estoult_fr-front
-
-docker build -f front.Dockerfile -t estoult_fr-front .
-docker compose up -d
---
+++ run-front_builder.sh
@@ -0,0 +1,6 @@
+docker container stop estoult_fr-front_builder
+docker container rm estoult_fr-front_builder
+docker image rm estoult_fr-front_builder
+
+docker build -f front_builder.Dockerfile -t estoult_fr-front_builder .
+docker compose up -d
