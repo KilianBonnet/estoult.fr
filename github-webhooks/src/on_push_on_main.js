f36b0159cd919a494faa7c8eb33908d1c8518c52
Kilian Bonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2023-12-10 18:41:35 +0100

    Aeration du code

--- github-webhooks/src/on_push_on_main.js
+++ github-webhooks/src/on_push_on_main.js
@@ -3,8 +3,16 @@ import { exec } from 'child_process';
 export function onPushOnMain(reply) {
     console.log("Received a push on main");
 
-    exec("cd .. && docker compose down && ./build-nginx.sh &&  && docker compose up -d", (error, stdout, stderr) => {
+    exec(
+    `
+    cd ..
+    docker compose down
+    ./build-nginx.sh
+    docker compose up -d
+    `,
+    (error, stdout, stderr) => {
         console.log(stdout);
         console.log(stderr);
+        if (error) console.error('Error:', error);
     });
 }
