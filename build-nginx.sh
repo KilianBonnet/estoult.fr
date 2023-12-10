1bb790088662a3f2abd4a4427f425b519a33b36d
Kilian Bonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2023-12-10 18:37:41 +0100

    Ca push

--- build-nginx.sh
+++ build-nginx.sh
@@ -2,4 +2,4 @@ docker container stop estoult_fr-nginx
 docker container rm estoult_fr-nginx
 docker image rm estoult_fr-nginx
 
-docker build -f nginx.Dockerfile -t estoult_fr-nginx .
+docker build --no-cache -f nginx.Dockerfile -t estoult_fr-nginx .
--- github-webhooks/src/on_push_on_main.js
+++ github-webhooks/src/on_push_on_main.js
@@ -3,7 +3,7 @@ import { exec } from 'child_process';
 export function onPushOnMain(reply) {
     console.log("Received a push on main");
 
-    exec("cd .. && docker compose down && docker compose up -d", (error, stdout, stderr) => {
+    exec("cd .. && docker compose down && ./build-nginx.sh &&  && docker compose up -d", (error, stdout, stderr) => {
         console.log(stdout);
         console.log(stderr);
     });
