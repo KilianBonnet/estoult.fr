8836d69ff82c06d7143139cdffdea9c5502b8406
Kilian Bonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2023-12-14 20:30:47 +0100

    atq

--- nginx/nginx-init.sh
+++ nginx/nginx-init.sh
@@ -16,7 +16,8 @@ echo estoult.fr will use cronjob autorenew
 touch /dev/certbot_logs
 echo "0 0 * * 0 root certbot renew --nginx >> /dev/certbot_logs" | crontab -
 crontab -l
-service cron restart
+# service cron restart
+atq
 
 
 # Start Nginx
