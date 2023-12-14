26ee0eb8cd86928b1ac2c2b6517a4c36c2e9efaa
Kilian Bonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2023-12-14 20:53:41 +0100

    sleep between certifications

--- nginx/nginx-init.sh
+++ nginx/nginx-init.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Check if /etc/letsencrypt/live/www.estoult.fr is empty
-# if [ -z "$(ls -A /etc/letsencrypt/live/www.estoult.fr)" ];
-# then certbot certonly --standalone --no-autorenew --agree-tos --no-eff-email --email kilian.bonnet@estoult.fr -d www.estoult.fr --redirect --keep-until-expiring
-# fi
+if [ -z "$(ls -A /etc/letsencrypt/live/www.estoult.fr)" ];
+then certbot certonly --standalone --no-autorenew --agree-tos --no-eff-email --email kilian.bonnet@estoult.fr -d www.estoult.fr --redirect --keep-until-expiring
+fi
 
-
+sleep 10
 
 # Check if /etc/letsencrypt/live/estoult.fr is empty
-# if [ -z "$(ls -A /etc/letsencrypt/live/estoult.fr)" ];
-# then certbot certonly --standalone --no-autorenew --agree-tos --no-eff-email --email kilian.bonnet@estoult.fr -d estoult.fr --redirect --keep-until-expiring
-# fi
+if [ -z "$(ls -A /etc/letsencrypt/live/estoult.fr)" ];
+then certbot certonly --standalone --no-autorenew --agree-tos --no-eff-email --email kilian.bonnet@estoult.fr -d estoult.fr --redirect --keep-until-expiring
+fi
 
 echo estoult.fr will use cronjob autorenew
 touch /dev/certbot_logs
