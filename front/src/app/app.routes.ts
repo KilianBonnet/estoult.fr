f4af518e039ef8c4ff4b3610596b8cacef43eeeb
Kilian Bonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2023-12-14 15:54:07 +0100

    Adding route title

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -3,8 +3,8 @@ import { HomeComponent } from './components/home/home.component';
 import { ChatComponent } from './components/chat/chat.component';
 
 export const routes: Routes = [
-    { path: '',  redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home',  component: HomeComponent },
-    { path: 'chat', component: ChatComponent },
-    { path: '**', redirectTo: 'home' }
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent, title: 'Estoult - Home' },
+  { path: 'chat', component: ChatComponent, title: 'Estoult - Chat' },
+  { path: '**', redirectTo: 'home' }
 ];
