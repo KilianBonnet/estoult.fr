f629a9faa4754c962a1a9b185f2d4643faaff010
Kilian Bonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2023-12-14 16:35:38 +0100

    Adding side pictures

--- front/src/app/components/side-picture/side-picture.component.ts
+++ front/src/app/components/side-picture/side-picture.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-side-picture',
   standalone: true,
@@ -8,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrl: './side-picture.component.css'
 })
 export class SidePictureComponent {
+  sidePictures: string[]  = ["flandre", "marisa", "reimu"];
+  currentPicturePath: string;
 
+  constructor() {
+    const currentPictureIndex: number = Math.floor(Math.random() * (this.sidePictures.length - 0)) + 0;
+    this.currentPicturePath = `./assets/side-picture-images/${this.sidePictures[currentPictureIndex]}.png`;
+  }
 }
